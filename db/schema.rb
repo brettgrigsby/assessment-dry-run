088fa8ae434688ee8ebaad7275de2143cbe1ab78
brettgrigsby <gbrett@gmail.com>
2015-09-23 09:56:36 -0600

    users with many links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923153708) do
+ActiveRecord::Schema.define(version: 20150923154244) do
+
+  create_table "links", force: :cascade do |t|
+    t.string   "url"
+    t.boolean  "read"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
