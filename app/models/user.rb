90898d661f8ebc74861df5551bcb34afd61e1942
brettgrigsby <gbrett@gmail.com>
2015-09-23 11:44:39 -0600

    user methods for read and unread links

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_many :links
   has_secure_password
   validates :email, presence: true, uniqueness: true
+
+
+  def read_links
+    links.where(read: true)
+  end
+
+  def unread_links
+    links.where(read: false)
+  end
 end
