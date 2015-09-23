ad8391eb14e0495ddada6a8ac1cff4fc3455dea0
brettgrigsby <gbrett@gmail.com>
2015-09-23 11:33:20 -0600

    links created and displayed

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,20 @@
 class LinksController < ApplicationController
 
   def create
+    link = Link.new(link_params)
+    link.user_id = current_user.id
+    if link.save
+      flash[:notice] = "link added!"
+    else
+      flash[:warning] = "invalid link!"
+    end
+    redirect_to current_user
+  end
 
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
   end
 
 end
